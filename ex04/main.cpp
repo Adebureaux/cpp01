3ec7f1d20a1aff4162cc7c5006d9cd2bd8967801
Augustin <adebureaux@gmail.com>
2022-04-02 01:27:21 +0200

    04 finshed

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -12,33 +12,33 @@ int main(int ac, char **av)
 		return (std::cout << "Error: cannot open the file '" << av[1] << "'" << std::endl || 1);
 	std::string const original((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
 	std::string const s1 = av[2];
+	if (!s1[0])
+		return (std::cout << "Error: second argument shouldn't be an empty string" << std::endl || 1);
 	std::string const s2 = av[3];
 	std::string copy;
 	file.close();
 	int i = 0;
-	int j = 0;
 	int find;
-	while (original[i])
+	while (1)
 	{
 		find = original.find(s1, i);
 		if (find != -1)
 		{
+			copy.append(original.substr(i, find - i));
 			copy.append(s2);
-			i += find;
+			i = find + s1.length();
 		}
 		else
 		{
-			copy.append(original);
-			i++;
+			copy.append(original.substr(i));
+			break;
 		}
 	}
-	std::cout << copy;
-
-	// Create a file a write the buffer inside
-	// std::string filename = av[1];
-	// std::ofstream outfile(filename.append(".replace"));
-	// if (!outfile.is_open())
-	// 	return (std::cout << "Error: cannot create the file '" << filename << "'" << std::endl || 1);
-	// outfile.close();
+	std::string filename = av[1];
+	std::ofstream outfile(filename.append(".replace"));
+	if (!outfile.is_open())
+		return (std::cout << "Error: cannot create the file '" << filename << "'" << std::endl || 1);
+	outfile << copy;
+	outfile.close();
 	return (0);	
 }
